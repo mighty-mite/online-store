ff2db8f9325e25832018e61e44098d08f99fca39
Ayur Tushinov <tushinov.ayur@gmail.com>
2024-01-31 22:40:41 +0300

    fix: get rid of comments, fix styles

--- src/pages/shopPage/ShopPage.tsx
+++ src/pages/shopPage/ShopPage.tsx
@@ -15,66 +15,43 @@ import {
 } from './shopPageSlice';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import Goods from '../../components/goods/Goods';
-import Service from '../../service/Service';
 import './shop.scss';
 import './filters.scss';
 
 function ShopPage() {
-  // const [category, setCategory] = useState<string[]>([]);
-  // const [brand, setBrand] = useState<string[]>([]);
-  // const [search, setSearch] = useState('');
-
-  // const [minPrice, setMinPrice] = useState<number>(1);
-  // const [maxPrice, setMaxPrice] = useState<number>(1);
-
   const dispatch = useAppDispatch();
   const { categories, brands, searchQuery, minPrice, maxPrice } =
     useAppSelector((state) => state.searchSettings);
 
   const categoryHandler = (str: string, isChecked: boolean) => {
     if (isChecked) {
-      // setCategory((state) => [...state, str]);
       dispatch(addCategory(str));
     } else {
-      // setCategory((state) => state.filter((item) => item !== str));
       dispatch(removeCategory(str));
     }
   };
 
   const brandHandler = (str: string, isChecked: boolean) => {
     if (isChecked) {
-      // setBrand((state) => [...state, str]);
       dispatch(addBrand(str));
     } else {
-      // setBrand((state) => state.filter((item) => item !== str));
       dispatch(removeBrand(str));
     }
   };
 
   const searchHandler = (str: string) => {
-    // setSearch(() => str.toLowerCase());
     dispatch(addSearchQuery(str.toLowerCase()));
   };
 
   useEffect(() => {
     dispatch(fetchPrices());
-    // const service = new Service();
-    // service
-    //   .getMinMaxPrices()
-    //   .then((arr) => {
-    //     setMinPrice(arr[0]);
-    //     setMaxPrice(arr[1]);
-    //   })
-    //   .catch();
   }, [dispatch]);
 
   const handleMinValue = (arg: number) => {
-    // setMinPrice(arg);
     dispatch(setMinPrice(arg));
   };
 
   const handleMaxValue = (arg: number) => {
-    // setMaxPrice(arg);
     dispatch(setMaxPrice(arg));
   };
 
